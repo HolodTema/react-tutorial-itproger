7bdb02ae77309874705777f3ccb784131ac23c48
HolodTema <art.holodilov@gmail.com>
2025-02-01 08:11:40 +0300

    Lesson 2 part 1 - jsx opportunities like jetpackCompose!

--- js/index.js
+++ js/index.js
@@ -1,7 +1,36 @@
+//without jsx:
+// ReactDOM.render(
+//     React.createElement(
+//         "input",
+//         {
+//             placeholder: "help text",
+//             onClick: () => console.log("clicked"),
+//             onMouseEnter: () => console.log("Mouse over")
+//         }
+//     ),
+//     document.getElementById("app")
+// );
+
+const inputClick = () => {
+    console.log("clicked")
+}
+
+const mouseOver = () => {
+    console.log("mouseOver")
+}
+
+const helpText = "Help text"
+
+const elements = <div className={}>
+    <h1>{helpText}</h1>
+    <input placeholder={helpText}
+           onClick={inputClick}
+           onMouseOver={mouseOver}/>
+    <p>{helpText === "Help text!" ? "Yes" : "No"}</p>
+</div>
+
+const app = document.getElementById("app")
 ReactDOM.render(
-    <div>
-        <h1>Hello</h1>
-        <h2>this tag is </h2>
-    </div>,
-    document.getElementById("app")
+    elements,
+    app
 );
