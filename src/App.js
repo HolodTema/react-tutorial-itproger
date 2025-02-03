7ea4cd442e7241ac067cb5121cd71b5dd9a401df
HolodTema <art.holodilov@gmail.com>
2025-02-03 13:04:02 +0300

    Lesson 4: React states and how to use it.

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import React from "react"
+import Header from "./components/Header"
+import Image from "./components/Image"
+import logo from "./img/logo.jpg"
+
+
+class App extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            helpText: "Help text",
+            userData: ""
+        }
+
+        this.inputClick = this.inputClick.bind(this)
+    }
+
+    helpText = "Help text"
+
+    render() {
+        return (
+            <div className="mydiv">
+                <Header title="Site header"/>
+                <Header title="lslsl"/>
+                <Header title="alhhahah"/>
+                <h1>{this.state.helpText}</h1>
+                <h2>{this.state.userData}</h2>
+                <input placeholder={this.state.helpText}
+                       onChange={
+                           event =>
+                               this.setState({userData: event.target.value})
+                       }
+                       onClick={this.inputClick}
+                       onMouseOver={this.mouseOver}/>
+                <p>{this.state.helpText === "Help text!" ? "Yes" : "No"}</p>
+                <Image image={logo}/>
+            </div>
+        )
+    }
+
+    inputClick() {
+        this.setState({helpText: "Changed"})
+        this.helpText = "Changed"
+        console.log("clicked")
+    }
+
+    mouseOver() {
+        console.log("mouseOver")
+    }
+}
+
+export default App
